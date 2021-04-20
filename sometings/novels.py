eb9cfb694d0d3238e864a893c0f08275d4b7ee66
Tutu <tuzhida2012@outlook.com>
2021-04-20 17:49:29 +0800

    更新了小说爬虫

--- sometings/novels.py
+++ sometings/novels.py
@@ -33,20 +33,28 @@ import requests, re, sys
 
 class downloader(object):
     pattern_catalog = re.compile(r'<dt>.*(\r|\n|\s)*</dt>')
-    pattern_body = re.compile(r'\(https://.*(\r|\n|\s)*.*m\.23xsww\.com')
+    # 去除尾部 以m.23xsww.com结尾的文本 例如 全本精彩小说尽在m.23xsww.com
+    # pattern_body = re.compile(r'\(https://.*(\r|\n|\s)*.*m\.23xsww\.com')
+    pattern_body = re.compile(r'\(https://.*(\r|\n|\s)*.*m\.cc148\.com')
 
     def __init__(self):
-        self.server = 'https://www.23xsww.com/'
-        self.target = 'https://www.23xsww.com/book/25030/25030968/'
+        # self.server = 'https://www.23xsww.com/'
+        self.server = 'https://www.cc148.com/'
+        # self.target = 'https://www.23xsww.com/book/25030/25030968/'
+        self.target = 'https://www.cc148.com/43_43648/'
         self.names = []     #存放章节名
         self.urls = []      #存放章节链接
         self.nums = 0       #章节数
+        self.bookName = '绝地求生捡碎片'
+        self.reqEncoding = 'gbk' # request请求时有些网站中文乱码 可以设置请求编码
     
     def get_download_url(self):
         req = requests.get(url=self.target)
+        req.encoding = self.reqEncoding
         html = req.text
         div_bf = BeautifulSoup(html, 'html.parser')
-        div = div_bf.find_all('div', class_='listmain')
+        # div = div_bf.find_all('div', class_='listmain')
+        div = div_bf.find_all('div', id='list')
         a_bf = BeautifulSoup(str(div[0]), 'html.parser')
         dd = self.pattern_catalog.split(str(a_bf.dl))[4].replace(r'\n</dl>','')
         dd_a_bf = BeautifulSoup(dd, 'html.parser')
@@ -54,15 +62,20 @@ class downloader(object):
         self.nums = len(a)
         for item in a:
             self.names.append(item.string)
-            self.urls.append(self.server + item.get('href'))
+            # self.urls.append(self.server + item.get('href'))
+            self.urls.append(self.target + item.get('href'))
 
     def get_contents(self, target):
         req = requests.get(url=target)
+        req.encoding = self.reqEncoding
         html = req.text
         bf = BeautifulSoup(html, 'html.parser')
-        texts = bf.find_all('div', class_='showtxt')
+        # texts = bf.find_all('div', class_='showtxt')
+        texts = bf.find_all('div', id='content')
+        # 用于减除 &nbsp;&nbsp;&nbsp;&nbsp; 这样的文本
         txt = texts[0].text.replace('\xa0'*8, "\n")
         really_txt = self.pattern_body.sub('\t', txt)
+        # print(txt)
         return really_txt
 
     def writer(self, name, path, text):
@@ -76,8 +89,9 @@ if __name__ == '__main__':
     dl = downloader()
     dl.get_download_url()
     print('<<小说小说>>开始下载：')
+    # print(dl.urls)
     for i in range(dl.nums):
-        dl.writer(dl.names[i], '七零福妻火辣辣.txt', dl.get_contents(dl.urls[i]))
+        dl.writer(dl.names[i], dl.bookName+".txt", dl.get_contents(dl.urls[i]))
         sys.stdout.write(" 已下载：%.3f%%" % float(i/dl.nums) + '\r')
         sys.stdout.flush()
     print('<<小说小说>>下载完成')
